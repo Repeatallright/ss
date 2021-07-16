1e7f9bc471ea2ee7b0a4f83d6dd447b45cd10e06
Repeatallright <levserg2003@gmail.com>
2021-07-16 18:22:13 +0300

    Beta 2

---
+++ Mogo/js/super.js
@@ -0,0 +1,36 @@
+let superBlock = document.querySelector('.super_block');
+let superHide = document.querySelector('.super_hide');
+let superImg = document.querySelector('.super_img');
+let imgView = document.querySelectorAll('.img_view');
+let superBut = document.querySelector('.super_but');
+
+for (let con = 0; con < imgView.length; con++) {
+    imgView[con].addEventListener('click', imgShow)
+
+}
+superImg.addEventListener('dblclick', superSize);
+
+superBut.addEventListener('click', () => {
+    superBlock.style.display = 'none';
+    console.log('aaa');
+    superBlock.classList.remove('super_size')
+})
+
+function superSize() {
+    superBlock.className.includes('super_size') ? superBlock.classList.remove('super_size') : superBlock.classList.add('super_size');
+    console.log(this.className.includes('super_size'));
+}
+
+
+
+function imgShow(event) {
+    let item = this.src;
+    this.classList.add('img_view_super')
+    item.slice(item.indexOf("L") + 2);
+    console.log(item);
+    superImg.src = item
+    superBlock.style.display = 'flex';
+}
+
+
+
