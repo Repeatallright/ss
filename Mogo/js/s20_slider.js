dcc93ee816ca9dc9041fee7638c041e94c511b76
Repeatallright <levserg2003@gmail.com>
2021-07-17 17:25:11 +0300

    Beta 5

--- Mogo/js/s20_slider.js
+++ Mogo/js/s20_slider.js
@@ -3,15 +3,18 @@ let blockSlider = document.querySelector('.review_block');
 let blockSliderAdd = document.querySelector('.review_slider_block');
 let sliderCount = 0;
 
+
+let slidersImg = ['person-img1', 'person-img2', 'person-img3'];
+let slidersName = ['Matthew Dix', 'Christopher Campbell', 'Michael Fertig']
+let slidersText = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut lекabore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum.", "Until recently, the prevailing view assumed lorem ipsum was born as a nonsense text. “It's not Latin, though it looks like it, and it actually says nothing,” Before & After magazine answered a curious reader, “Its ‘words’ loosely approximate the frequency with which letters occur in English, which is why at a glance it looks pretty real.”"]
+
 buttons.forEach(elem => {
     elem.addEventListener('click', sliderChange)
 })
 
 function sliderChange(event) {
     let idItem = event.target.id;
-    let slidersImg = ['person-img1', 'person-img2', 'person-img3'];
-    let slidersName = ['Matthew Dix', 'Christopher Campbell', 'Michael Fertig']
-    let slidersText = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut lекabore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum.", "Until recently, the prevailing view assumed lorem ipsum was born as a nonsense text. “It's not Latin, though it looks like it, and it actually says nothing,” Before & After magazine answered a curious reader, “Its ‘words’ loosely approximate the frequency with which letters occur in English, which is why at a glance it looks pretty real.”"]
+
     idItem == 'but_left' ? sliderCount-- : sliderCount++;
     sliderCount > slidersImg.length - 1 ? sliderCount = 0 : sliderCount;
     sliderCount == -1 ? sliderCount = 2 : sliderCount;
